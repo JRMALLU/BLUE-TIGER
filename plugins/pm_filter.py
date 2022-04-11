0fd445ef2e1a56338722333129ec0a0406ae778f
Movie World <100509655+ATHIF-EFX@users.noreply.github.com>
2022-04-11 15:34:29 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -619,32 +619,6 @@ async def cb_handler(client: Client, query: CallbackQuery):
             text=script.PURGE_TXT,
             reply_markup=reply_markup,
             parse_mode='html'
-         )
-     elif query.data == "pin":
-        buttons = [[
-            InlineKeyboardButton('𝙱𝙰𝙲𝙺', callback_data='about')
-        ]]
-        reply1 = await query.message.reply_text(
-            text="▢▢▢"
-        )
-        await asyncio.sleep(0.5)
-        reply2 = await reply1.edit_text(
-            text="▣▢▢"
-        )
-        await asyncio.sleep(0.5)
-        reply3 = await reply2.edit_text(
-            text="▣▣▢"
-        )
-        await asyncio.sleep(0.5)
-        reply4 = await reply3.edit_text(
-            text="▣▣▣"
-        )
-        await reply4.delete()
-        reply_markup = InlineKeyboardMarkup(buttons)
-        await query.message.edit_text(
-            text=script.PIN_TXT
-            reply_markup=reply_markup,
-            parse_mode='html'
         )
     elif query.data == "songs":
         buttons = [[
