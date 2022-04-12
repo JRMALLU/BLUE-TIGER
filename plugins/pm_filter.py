9c602b67d54cae9c7310d9ece43781c04f313dcf
Movie World <100509655+ATHIF-EFX@users.noreply.github.com>
2022-04-12 09:55:16 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -593,7 +593,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await reply4.delete()
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            text=script.MAMMOKKA_TXT,
+            text=script.MAMMOKA_TXT,
             reply_markup=reply_markup,
             parse_mode='html'
         )
