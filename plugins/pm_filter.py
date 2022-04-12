229249a6dada476792c511b2b36419c874f8413b
Movie World <100509655+ATHIF-EFX@users.noreply.github.com>
2022-04-12 14:54:30 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -829,26 +829,6 @@ async def cb_handler(client: Client, query: CallbackQuery):
             reply_markup=reply_markup,
             parse_mode='html'
         )
-    reply1 = await query.message.reply_text(
-            text="⭗ ⭗ ⭗ ⭗ ⭗ ⭗"
-        )
-        await asyncio.sleep(0.5)
-        reply2 = await reply1.edit_text(
-            text="⦿ ⦿ ⦿ ⭗ ⭗ ⭗"
-        )
-        await asyncio.sleep(0.5)
-        reply3 = await reply2.edit_text(
-            text="⦿ ⦿ ⦿ ⦿ ⭗ ⭗"
-        )
-        await asyncio.sleep(0.5)
-        reply4 = await reply3.edit_text(
-            text="⦿ ⦿ ⦿ ⦿ ⦿ ⭗"
-        )
-        await asyncio.sleep(0.5)
-        reply5 = await reply4.edit_text(
-            text="⦿ ⦿ ⦿ ⦿ ⦿ ⦿"
-        )
-        await reply5.delete()
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
             text=script.BUTTON_TXT,
