e388c463d3b2628a7f0e1f230df2042843fbb30c
Movie World <100509655+ATHIF-EFX@users.noreply.github.com>
2022-04-13 22:21:37 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -526,6 +526,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('🧔𝙼𝙰𝙼𝙼𝙾𝙺𝙺𝙰🧔', callback_data='mammokka'),
             InlineKeyboardButton('💠𝚂𝚃𝙸𝙲𝙺𝙴𝚁 𝙸𝙳💠', callback_data='id')
             ],[
+            InlinekeyboardButton('📁𝙰𝚄𝚃𝙾 𝙵𝙸𝙻𝚃𝙴𝚁 𝙾𝙽📁', callback_data='on'),
+            ],[
             InlineKeyboardButton('𝙷𝙾𝙼𝙴', callback_data='start'),
             InlineKeyboardButton('𝚂𝚃𝙰𝚃𝚂', callback_data='stats')
         ]]
@@ -608,6 +610,32 @@ async def cb_handler(client: Client, query: CallbackQuery):
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
             text=script.SOURCE_TXT,
+            reply_markup=reply_markup,
+            parse_mode='html'
+        )
+    elif query.data == "on":
+        buttons = [[
+            InlineKeyboardButton('𝙱𝙰𝙲𝙺', callback_data='help')
+        ]]
+        reply1 = await query.message.reply_text(
+            text="▢▢▢"
+        )
+        await asyncio.sleep(0.5)
+        reply2 = await reply1.edit_text(
+            text="▣▢▢"
+        )
+        await asyncio.sleep(0.5)
+        reply3 = await reply2.edit_text(
+            text="▣▣▢"
+        )
+        await asyncio.sleep(0.5)
+        reply4 = await reply3.edit_text(
+            text="▣▣▣"
+        )
+        await reply4.delete()
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await query.message.edit_text(
+            text=script.FILTER_TXT,
             reply_markup=reply_markup,
             parse_mode='html'
         )
