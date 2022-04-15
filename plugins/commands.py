0bc237b9898f3ee0d7d264d508e55e827e19a547
Movie World <100509655+ATHIF-EFX@users.noreply.github.com>
2022-04-15 14:17:25 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -72,7 +72,7 @@ async def start(client, message):
         await message.reply_chat_action("typing")
         await message.reply_photo(
             photo=random.choice(PICS),
-            caption=script.START_TXT.format(get, message.from_user.mention, temp.U_NAME, temp.B_NAME),
+            caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode='html'
         )
@@ -121,7 +121,7 @@ async def start(client, message):
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
             photo=random.choice(PICS),
-            caption=script.START_TXT.format(get, message.from_user.mention, temp.U_NAME, temp.B_NAME),
+            caption=script.START_TXT.format(message.from_user.mention, message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode='html'
         )
